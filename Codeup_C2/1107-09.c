ecaa367ec59ecf76b4541d7015960db6ca2abe69
S510U <fx0605@naver.com>
2024-12-07 19:56:38 +0900

    1day

---
+++ Codeup_C2/1107-09.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int main(void)
+//{
+//	int i;
+//	for (i = 0; i < 50; i++) {
+//		printf("print");
+//	}
+//	return 0; // 1107 반복 출력하기 1
+//}
+
+//int main(void)
+//{
+//	int i = 0;
+//	for (i = 0; i < 20; i++) {
+//		printf("hello");
+//	}
+//	printf(" ");
+//	for (i = 0; i < 30; i++) {
+//		printf("world");
+//	}
+//	return 0; // 1108 반복 출력하기 2
+//}
+
+//int main(void)
+//{
+//	char name[21], code[21];
+//	double key;
+//	int age;
+//	scanf("%s\n", name);
+//	scanf("%d\n", &age);
+//	scanf("%s\n", code);
+//	scanf("%lf", &key);
+//
+//	printf("%s\n%d\n%s\n%g", name, age, code, key);
+//	return 0; // 1109 보안카드 접수증
+//}
