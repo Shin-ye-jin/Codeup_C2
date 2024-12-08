b7e1252a397b12e9ef3e828558dcd1abb06e104d
S510U <fx0605@naver.com>
2024-12-08 14:54:35 +0900

    2day

---
+++ Codeup_C2/112n.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int main(void)
+//{
+//	int a, b;
+//	scanf("C%dH%d", &a, &b);
+//	printf("%d", a * 12 + b);
+//	return 0; // 1124 분자량 구하기 1
+//}
+
+//int main(void)
+//{
+//	int a, b;
+//	scanf("%d %d", &a, &b);
+//
+//	printf("%d + %d = %d\n", a, b, a + b);
+//	printf("%d - %d = %d\n", a, b, a - b);
+//	printf("%d * %d = %d\n", a, b, a * b);
+//	printf("%d / %d = %d\n", a, b, a / b);
+//	printf("%d %% %d = %d", a, b, a % b);
+//	return 0; // 1126 정수 계산기
+//}
+//
+//int main(void)
+//{
+//	float a, b,sum=0;
+//	int i;
+//
+//	for (i = 0; i < 3; i++) {
+//		scanf("%f %f", &a, &b);
+//		sum = sum + a * b;
+//	}
+//	printf("%.1f", sum);
+//	return 0; // 1127 성적 계산
+//}
