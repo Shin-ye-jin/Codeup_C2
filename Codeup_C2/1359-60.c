9657a8c8044ed43091cea5ad11844095b9924f11
S510U <fx0605@naver.com>
2024-12-16 21:10:19 +0900

    // 6day

---
+++ Codeup_C2/1359-60.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int main(void)
+//{
+//	int n, i, j;
+//	scanf("%d", &n);
+//
+//	for (i = 1; i <= n; i++) {
+//		for (j = 1; j <= i; j++) {
+//			printf("%d ", j);
+//		}
+//		printf("\n");
+//	}
+//	return 0; // 1359 숫자 피라미드 1
+//}
+
+//int main(void)
+//{
+//	int n, i, j;
+//	scanf("%d", &n);
+//
+//	for (i = n; i >= 1; i--) {
+//		for (j = 0; j < i; j++) {
+//			printf("%d ", i);
+//		}
+//		printf("\n"); // 1360 숫자 피라미드 2
+//	}
+//}
