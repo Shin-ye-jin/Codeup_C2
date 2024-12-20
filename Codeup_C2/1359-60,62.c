2fd1579d39222804cbdf8ca921a7d32a8902f5ca
S510U <fx0605@naver.com>
2024-12-20 21:00:35 +0900

    6day

--- Codeup_C2/1359-60,62.c
+++ Codeup_C2/1359-60,62.c
@@ -27,3 +27,30 @@
 //		printf("\n"); // 1360 숫자 피라미드 2
 //	}
 //}
+
+//int r(int n) {
+//	if (n== 1) {
+//		return 1;
+//	}
+//	else {
+//		return n + r(n - 1);
+//	}
+//}
+//
+//int main(void)
+//{
+//	int n, i, j, re = 0;
+//	scanf("%d", &n);
+//
+//	re = r(n);
+//
+//	for (i = 1; i <= n; i++) {
+//		for (j = 1; j <= i; j++) {
+//			printf("%d ", re);
+//			re--;
+//		}
+//		printf("\n");
+//	}
+//
+//	return 0; // 1362 숫자 피라미드 3
+//}
